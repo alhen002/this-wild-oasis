85fc9aef5914127e6495841d81c1e7ee534a7391
Alexander Henting <a.henting@posteo.de>
2023-11-22 18:36:50 +0100

    added react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,46 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Users from "./pages/Users.jsx";
+import Settings from "./pages/Settings.jsx";
+import Account from "./pages/Account.jsx";
+import Login from "./pages/Login.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 import { GlobalStyles } from "./styles/GlobalStyles.js";
-import Button from "./ui/Button.jsx";
-import Input from "./ui/Input.jsx";
-import Heading from "./ui/Heading.jsx";
-import Row from "./ui/Row.jsx";
-const StyledApp = styled.main`
-  padding: 20px;
-`;
+import AppLayout from "./ui/AppLayout.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-      <StyledApp>
-        <Row type={"vertical"}>
-          <Row type={"horizontal"}>
-            <Heading as={"h1"}>The Wild Oasis</Heading>
-            <div>
-              <Heading as={"h2"}>Check in & Out</Heading>
-              <Button variant={"primary"} size={"medium"}>
-                Check In
-              </Button>
-              <Button variant={"secondary"} size={"small"}>
-                Check Out
-              </Button>
-            </div>
-          </Row>
-          <Row type={"vertical"}>
-            <Heading as={"h3"}>Form</Heading>
-            <form>
-              <Input type={"number"} placeholder={"Number of Guests"} />
-              <Input type={"number"} placeholder={"Number of Guests"} />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to={"dashboard"} />} />
+            <Route path={"dashboard"} element={<Dashboard />} />
+            <Route path={"bookings"} element={<Bookings />} />
+            <Route path={"cabins"} element={<Cabins />} />
+            <Route path={"users"} element={<Users />} />
+            <Route path={"settings"} element={<Settings />} />
+            <Route path={"account"} element={<Account />} />
+          </Route>
+          <Route path={"login"} element={<Login />} />
+          <Route path={"*"} element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
-
 export default App;
