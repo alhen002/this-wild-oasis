bcba34078fac561e4168d8ec466a6a1306d9a971
Alexander Henting <a.henting@posteo.de>
2023-11-25 11:33:33 +0100

    added filter functionality

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +34,28 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+  const currentValue = searchParams.get(filterField) || options[0].value;
+
+  return (
+    <StyledFilter>
+      {options?.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentValue}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
